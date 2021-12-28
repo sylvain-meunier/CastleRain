08a79521687aa6d0b25425c92ffbb28e679389af
sylvain-meunier <sysyl.meunier@hotmail.fr>
2021-12-29 00:45:17 +0100

    pyliste module v2

--- Code/Modules/pyliste.ml
+++ Code/Modules/pyliste.ml
@@ -12,7 +12,25 @@ struct
 			p.mem.(p.taille) <- ele;
 			p.taille <- p.taille + 1;
 		end ;
-	and set p ele ind = p.mem.(ind) <- ele
+	and remove p ind =
+		if ind >= 0 && p.taille > 0 then
+			begin
+				for i=ind+1 to p.taille-1 do
+					p.mem.(i-1) <- p.mem.(i)
+				done;
+				p.taille <- p.taille - 1;
+			end ;
+	and set p ind ele = p.mem.(ind) <- ele
 	and get p ind = p.mem.(ind)
 	and new_pyliste ele = {mem = (Array.make 1 ele); taille = 0; ele = ele}
+	let insert p ind ele =
+		if ind >= 0 then
+			begin
+				if p.taille > 0 then add p (p.mem.(p.taille-1))
+				else add p ele;
+				for i=p.taille-1 downto ind+1 do
+					p.mem.(i) <- p.mem.(i-1)
+				done;
+				set p ind ele;
+			end ;
 end ;;
